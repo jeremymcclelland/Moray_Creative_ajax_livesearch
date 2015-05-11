27708d7fa3648cf3714ff5ace5666f5e87475427
moraycreative <jeremy@moraycreative.com>
2015-05-11 11:48:53 -0400

    updated ajax call
    changed output to name value

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 $.getJSON('data.json', function(data) {
-	console.log(data);
-
+	var output = '<ul class = "searchresults">';
+	$.each(data, function(key, val) {
+		output += '<li>';
+		output += '<h2>' + val.name + '</h2>';
+		output =+ '</li>';
+	});
+	output += '</ul'>;
+	$('#update').html(output);
 });//get JSON
