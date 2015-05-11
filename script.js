cc12ba6f9d8614480ef6d16901857c520467ea2a
moraycreative <jeremy@moraycreative.com>
2015-05-11 11:56:41 -0400

    updated output in script

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ $.getJSON('data.json', function(data) {
 	$.each(data, function(key, val) {
 		output += '<li>';
 		output += '<h2>' + val.name + '</h2>';
+		output += '<img src="images/' + val.shortname + '_tn.jpg" alt="' + val.name + '" />';
+		output += '<p>' + val.bio + '</p>';
 		output += '</li>';
 	});
 	output += '</ul>';
